99a50a8a7a78b0f5e8242036086cce4911cd8836
Marlon Martins <marlon-martins@outlook.com>
2024-07-24 12:29:04 +0200

    Redux Toolkit Tutorial - 24 - Async Thunks

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -3,19 +3,17 @@ const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions;
 const fetchUsers = require('./features/user/userSlice');
 
-console.log("Initial state", store.getState());
-const unsubscribe = store.subscribe(() => {});
-
-store.dispatch(fetchUsers());
-
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.restocked(3));
-
-// store.dispatch(icecreamActions.ordered());
-// store.dispatch(icecreamActions.ordered());
-// store.dispatch(icecreamActions.restocked(2));
-
-
-// unsubscribe();
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated State ', store.getState())
+})
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(3))
+store.dispatch(fetchUsers())
+unsubscribe()
